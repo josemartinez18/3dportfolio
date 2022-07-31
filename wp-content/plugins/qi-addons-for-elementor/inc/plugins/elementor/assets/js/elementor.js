f1af4d55dc48502330f393d36e52feae44c1aa76
josemartinez18 <j.f.martinezcastro@tudelft.nl>
2022-07-31 20:42:30 +0200

    July 31 update

--- wp-content/plugins/qi-addons-for-elementor/inc/plugins/elementor/assets/js/elementor.js
+++ wp-content/plugins/qi-addons-for-elementor/inc/plugins/elementor/assets/js/elementor.js
@@ -1,9 +1,12 @@
 (function ( $ ) {
 	'use strict';
+
 	$( window ).on(
 		'elementor/frontend/init',
 		function () {
 			qodefAddonsElementor.init();
+			qodefAddonsElementorPromoWidgets.init();
+
 		}
 	);
 
@@ -45,6 +48,116 @@
 					}
 				}
 			);
+		},
+	};
+
+	var qodefAddonsElementorPromoWidgets = {
+		init: function () {
+			if ( typeof elementor !== 'undefined' ) {
+				elementor.hooks.addFilter(
+					'panel/elements/regionViews',
+					function ( panel ) {
+						var qodeWidgetsPromoHandler,
+							elementsView   = panel.elements.view,
+							categoriesView = panel.categories.view,
+							widgets        = panel.elements.options.collection,
+							categories     = panel.categories.options.collection;
+
+						qodeWidgetsPromoHandler = {
+
+							isQodeWidget: function () {
+
+								if ( undefined !== this.model.get( 'name' ) ) {
+									return 0 === this.model.get( 'name' ).indexOf( 'qi_' );
+								}
+							},
+
+							getElementObj: function ( key ) {
+
+								var widgetObj = elementor.config.promotionWidgets.find(
+									function ( widget, index ) {
+										if ( widget.name == key ) {
+											return true;
+										}
+									}
+								);
+
+								return widgetObj;
+
+							},
+
+							onMouseDown: function () {
+
+								var actionURL = elementor.config.elementPromotionURL.replace(
+										'%s',
+										this.model.get( 'name' )
+									),
+									title     = this.model.get( 'title' ),
+									message   = sprintf(
+										wp.i18n.__(
+											'Use %s widget and dozens more pro features to extend your toolbox and build sites faster and better.',
+											'qi-addons-for-elementor'
+										),
+										title
+									);
+
+								if ( this.isQodeWidget() ) {
+									var widgetObject = this.getElementObj( this.model.get( 'name' ) );
+									if ( typeof widgetObject.helpUrl !== 'undefined' ) {
+										actionURL = widgetObject.helpUrl;
+									}
+
+									message = sprintf(
+										wp.i18n.__(
+											'The %s comes with advanced professional functionalities and an even smoother website-making experience. %s Upgrade Qi Addons for Elementor %s',
+											'qi-addons-for-elementor'
+										),
+										title,
+										'<a class="qodef-dialog-box-link" target="_blank" href="https://qodeinteractive.com/qi-addons-for-elementor/pricing/">',
+										'</a>'
+									);
+								}
+
+								elementor.promotion.showDialog(
+									{
+										/* translators: %s: Widget Title. */
+										headerMessage: sprintf(
+											wp.i18n.__(
+												'%s Widget',
+												'qi-addons-for-elementor'
+											),
+											title
+										),
+										message: message,
+										top: '-7',
+										element: this.el,
+										actionURL: actionURL
+									}
+								);
+
+							}
+						};
+
+						panel.elements.view = elementsView.extend(
+							{
+								childView: elementsView.prototype.childView.extend( qodeWidgetsPromoHandler )
+							}
+						);
+
+						panel.categories.view = categoriesView.extend(
+							{
+								childView: categoriesView.prototype.childView.extend(
+									{
+										childView: categoriesView.prototype.childView.prototype.childView.extend( qodeWidgetsPromoHandler )
+									}
+								)
+							}
+						);
+
+						return panel;
+					}
+				);
+			}
 		},
 	};
 
